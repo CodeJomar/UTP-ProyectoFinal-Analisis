2d8f93473132755d0e9970d337da88a9251259c2
CodeJomar <codejomar@gmail.com>
2025-07-19 18:17:17 -0500

    Modelos
    Creando modelos

---
+++ src/main/java/com/udemy/matriculas/registros/models/entities/Docente.java
@@ -0,0 +1,26 @@
+package com.udemy.matriculas.registros.models.entities;
+
+import com.udemy.matriculas.auth.models.entities.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "docentes")
+public class Docente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private Usuario usuario;
+    
+    
+    
+}
---
+++ src/main/java/com/udemy/matriculas/registros/models/entities/Estudiante.java
@@ -0,0 +1,36 @@
+package com.udemy.matriculas.registros.models.entities;
+
+import com.udemy.matriculas.auth.models.entities.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "estudiantes")
+public class Estudiante {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private Usuario usuario;
+    
+    @Column(name = "nombres", nullable = false)
+    private String nombre;
+    
+    @Column(name = "apellidos", nullable = false)
+    private String apellido;
+    
+    @Column(name = "celulares", nullable = false)
+    private String celular;
+    
+    @Column(name = "documento_identidad", nullable = false)
+    private String dni;
+    
+}
